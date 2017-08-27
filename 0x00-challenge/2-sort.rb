aa3b61938580355c21661f571e756ff0934e789e
cnov20 <128@holbertonschool.com>
2017-08-27 03:31:23 +0000

    Re-added 'puts' call to print output - not sure how this got removed

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,6 +1,6 @@
 ###
 #
-#  Sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
 
@@ -11,7 +11,7 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
@@ -27,3 +27,5 @@ ARGV.each do |arg|
     end
     result << i_arg if !is_inserted
 end
+
+puts result
